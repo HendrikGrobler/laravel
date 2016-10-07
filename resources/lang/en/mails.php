5f4b7f532e3b777ab7dc02355fbe011a033e8408
Hendrik Grobler <hendrikg@miway.co.za>
2016-10-07 12:01:37 +0200

    Add missing trailing comma

--- resources/lang/en/mails.php
+++ resources/lang/en/mails.php
@@ -13,5 +13,5 @@ return [
         'intro_line' => 'You are receiving this email because we received a password reset request for your account.',
         'reset_action' => 'Reset Password',
         'outro_line' => 'If you did not request a password reset, no further action is required.',
-    ]
+    ],
 ];

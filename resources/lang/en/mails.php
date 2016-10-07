a0f2a69890a423107f27fbb0557429fae1a327f4
Hendrik Grobler <hendrikg@miway.co.za>
2016-10-07 11:49:58 +0200

    Add 'en' language file with translations for default email template and password reset email

---
+++ resources/lang/en/mails.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'default' => [
+        'greeting' => "Hello!",
+        'error_greeting' => "Whoops!",
+        'salutation' =>  "Regards,\n:appName",
+        'action_sub_copy' => "If you’re having trouble clicking the :actionText button, copy and paste the URL below into your web browser:",
+        'footer_copyright_year' => "&copy; :year",
+        'footer_copyright' => "All rights reserved.",
+    ],
+    'password_reset' => [
+        'intro_line' => "You are receiving this email because we received a password reset request for your account.",
+        'reset_action' => "Reset Password",
+        'outro_line' => "If you did not request a password reset, no further action is required.",
+    ]
+];
